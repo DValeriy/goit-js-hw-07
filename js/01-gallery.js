90790ea9d68d3f5f97863b0090f409d46d013659
DValeriy <valeriydm20@gmail.com>
2021-09-28 19:50:52 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryNode=document.querySelector(".gallery")
+const linkNode = document.querySelector(".gallery a")
+
+let galleryListEl = galleryItems.map(item =>
+`<div class="gallery__item">
+<a class="gallery__link" href='${item.original}''>
+  <img
+    class="gallery__image"
+    src='${item.preview}'
+    data-source='${item.original}'
+    alt='${item.description}'
+  />
+</a>
+</div>`
+).join("");
+
+galleryNode.insertAdjacentHTML("beforeend", galleryListEl)
+let instance;
+
+galleryNode.addEventListener("click", e => {
+  if (e.target.tagName !== "IMG") { return false };
+
+  e.preventDefault();
+  // 1 method
+  let linkImg = e.target.dataset.source;
+  instance = basicLightbox.create(`<img
+    src='${linkImg}'
+    data-source='https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg'
+  />`)
+  
+  // 2 method
+  // let img = e.target;
+  // img.src = linkImg
+  // const instance = basicLightbox.create(img.outerHTML)
+  
+  instance.show()
+  const handler = e => {
+    if (e.key === "Escape") {
+      onClose: (instance) => { document.removeEventListener("keydown", handler) };
+      instance.close();
+    }
+  }
+  document.addEventListener("keydown", handler);
+  // document.removeEventListener("keydown", handler)
+
+})
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryNode=document.querySelector(".gallery")
+const linkNode = document.querySelector(".gallery a")
+
+let galleryListEl = galleryItems.map(item =>
+`<a class="gallery__item" href='${item.original}''>
+  <img
+    class="gallery__image"
+    src='${item.preview}'
+    alt='${item.description}'
+  />
+</a>`
+).join("");
+
+galleryNode.insertAdjacentHTML("beforeend", galleryListEl)
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
